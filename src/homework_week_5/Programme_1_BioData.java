d7ac9b85d608d2a77a9e13f41c407146b55f3556
jignaneeraj <jignaneeraj@gmail.com>
2023-10-18 23:43:04 +0100

    jigna homework

---
+++ src/homework_week_5/Programme_1_BioData.java
@@ -0,0 +1,23 @@
+package homework_week_5;
+
+/**
+ * 1. Write a program that prints your BIO Data.
+  */
+public class Programme_1_BioData {
+    public static void main(String[] args){
+        System.out.println("                       BIO DATA                                    ");
+        System.out.println("Name                   : James Gosling");
+        System.out.println("Mobile                 : 0900 999999");
+        System.out.println("Email                  : james.gosling@java.com");
+        System.out.println("Gender                 : Male");
+        System.out.println("Date of Birth          : 19.05.1955");
+        System.out.println("Marital Status         : Single");
+        System.out.println("Languages Known        : Java, C+, Python, Ruby");
+        System.out.println("Qualification          : Master of Invention in Computer Programming");
+        System.out.println("Experience             : Head of change in Software Development ");
+        System.out.println("Address                : Cornwall, Ontario, Burlington, Silicon Valley");
+        System.out.println("Place                  : Always Online ");
+        System.out.println("Date                   : 31.12.2000");
+        System.out.println("Signature              : _()_");
+    }
+}
---
+++ src/homework_week_5/Programme_4_RightTrianlge.java
@@ -0,0 +1,24 @@
+package homework_week_5;
+
+
+    /**
+     * 4. Write a Java Program to print below triangle
+     *
+     *  *
+     *  **
+     *  ***
+     *  ****
+     *  *****
+     *  ******
+     */
+    public class Programme_4_RightTrianlge {
+        public static void main(String[] args){
+            System.out.println("*");
+            System.out.println("**");
+            System.out.println("***");
+            System.out.println("****");
+            System.out.println("*****");
+            System.out.println("******");
+
+        }
+    }
